dd0db4685972c779cbbe9cc0ca2b13d780c52103
jkjh8 <jkjh82@naver.com>
2022-05-07 15:09:30 +0900

    set icon

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,7 @@
 <template>
-  <q-page class="flex flex-center"> first pages </q-page>
+  <q-page class="flex flex-center"
+    ><q-icon name="svguse:icons.svg#logo" size="200px"
+  /></q-page>
 </template>
 
 <script>
